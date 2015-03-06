b4062b35100044338c3968faba997168a9e7bb89
Noel Killebrew <noel.killebrew@placewise.com>
2015-03-06 09:53:29 -0700

    Bridgeport: yesterdays updates

--- bridgeport/src/js/search-toggle.js
+++ bridgeport/src/js/search-toggle.js
@@ -1,25 +1,7 @@
 // Search Toggle
-$(document).ready(function() {
-
-    $('.search-toggle').onClick
-
-    // placewiseLP.search.timeout;
-    // placewiseLP.search.closeSearch = function() { 
-    //     $('#search-box i').removeClass('active');
-    //     $('#search').slideUp(); 
-    // }
-    // $("#search-box i").on('mouseenter', function() {
-    //     $(this).addClass('active');
-    //     clearTimeout(placewiseLP.search.timeout);
-    //     $('#search').slideDown();
-    // })
-    // $('header').on('mouseleave', function() {
-    //     if ($('#search-box #search input[type="text"]').is(':focus') !== true) {
-    //         placewiseLP.search.timeout = setTimeout(placewiseLP.search.closeSearch, 500);
-    //     }
-    // });
-    // $('#search-box #search input[type="text"]').blur(function() {
-    //     placewiseLP.search.closeSearch();
-    // });
-
-)};
+$(function() {
+    $('.search-toggle').click(function() {
+        $(this).toggleClass('active');
+        $(".search-box").slideToggle("slow");
+    });
+});
